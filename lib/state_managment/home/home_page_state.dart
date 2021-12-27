737d847679415fbb91728fa7487a668237669ee8
shnaseri <57701223+shnaseri@users.noreply.github.com>
2021-12-27 13:26:40 +0330

    build(feature): add api of home page

---
+++ lib/state_managment/home/home_page_state.dart
@@ -0,0 +1,10 @@
+part of 'home_page_cubit.dart';
+
+@immutable
+abstract class HomePageState {}
+
+class HomePageInitial extends HomePageState {}
+
+class HomePageLoading extends HomePageState {}
+
+class HomePageLoad extends HomePageState {}
